8d0fd6bf887d6dee32af4010709b2cfeddf8eddb
‘kumaonigiri’ <monmonda25@gmail.com>
2021-03-07 00:54:03 +0900

    first commit

--- wp-content/themes/searchform.php
+++
@@ -1,5 +0,0 @@
-<?php if ( ! defined( 'ABSPATH' ) ) exit; ?>
-<form role="search" method="get" class="c-searchForm" action="<?=esc_url( HOME )?>" role="search">
-	<input type="text" value="" name="s" class="c-searchForm__s s" placeholder="検索" aria-label="検索ワード">
-	<button type="submit" class="c-searchForm__submit icon-search hov-opacity u-bg-main" value="search" aria-label="検索を実行する"></button>
-</form>
